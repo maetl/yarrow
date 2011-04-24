09acd2d6a786a06ddd97d194f18950b6ff3e7b26
maetl <me@maetl.net>
2011-04-24 11:22:55 +1000

    adding copyright/license header to files

--- Yarrow/ObjectModel.php
+++ Yarrow/ObjectModel.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Yarrow
+ * Simple Documentation Generator
+ * <http://yarrowdoc.org>
+ *
+ * Copyright (c) 2010-2011, Mark Rickerby <http://maetl.net>
+ * All rights reserved.
+ * 
+ * This library is free software; refer to the terms in the LICENSE file found
+ * with this source code for details about modification and redistribution.
+ */
 
 class ObjectModel {
 	
