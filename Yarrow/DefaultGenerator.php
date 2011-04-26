b0ed34b2d2a04e956231db771c5c97f0ca651488
maetl <me@maetl.net>
2011-04-26 15:46:01 +1000

    moving default theme to subdir and bundling with package, hopefully fixes #4

--- Yarrow/DefaultGenerator.php
+++ Yarrow/DefaultGenerator.php
@@ -37,6 +37,6 @@ class DefaultGenerator extends Generator {
 	}
 	
 	protected function getConverter() {
-		return new PHPConverter('Themes/Default');
+		return new PHPConverter('Yarrow/Themes/Default');
 	}	
 }
