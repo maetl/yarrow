95798fcbed16f028624014048e4bebad29a83144
maetl <me@maetl.net>
2011-05-01 03:18:51 +1000

    clear

---
+++ Yarrow/Themes/Plain/class.tpl.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html lang="en">
+  <head>
+    <title><?php echo $meta['title']; ?></title>
+  </head>
+  <body>
+	
+	
+	<div><a href="../index.html">Index</a></div>
+	
+	<h1><?php echo $class; ?></h1>
+
+	<p><?php echo $class->summary; ?></p>
+	
+	<p>In File: <a href="../<?php echo $class->file->getRelativeLink(); ?>"><?php echo $class->file->name; ?></a></p>
+
+	<?php foreach($class->methods as $method): ?>
+
+		<h2><?php echo $method; ?></h2>
+		<p><?php echo $method->summary; ?></p>
+
+
+	<?php endforeach ?>
+	
+	
+  </body>
+</html>
+
---
+++ Yarrow/Themes/Plain/index.tpl.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html lang="en">
+  <head>
+    <title><?php echo $meta['title']; ?></title>
+  </head>
+  <body>
+
+	<h1><?php echo $meta['title']; ?></h1>
+
+	<ul>
+	<?php foreach($objectModel->classes as $class): ?>
+		<li><a href="<?php echo $class->relativeLink; ?>.html"><?php echo $class; ?></li>
+	<?php endforeach ?>
+	</ul>
+
+  </body>
+</html>
+
