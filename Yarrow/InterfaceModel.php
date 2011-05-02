451dee302317aab7a5e5237cf3247e66375b846f
maetl <me@maetl.net>
2011-05-02 20:34:09 +1000

    adding interface model

---
+++ Yarrow/InterfaceModel.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Yarrow {{version}}
+ * Simple Documentation Generator
+ * <http://yarrowdoc.org>
+ *
+ * Copyright (c) 2010-2011, Mark Rickerby <http://maetl.net>
+ * All rights reserved.
+ * 
+ * This library is free software; refer to the terms in the LICENSE file found
+ * with this source code for details about modification and redistribution.
+ */
+
+class InterfaceModel extends CodeModel {
+
+	
+	
+}
