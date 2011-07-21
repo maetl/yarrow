6079af64539bbff804f43fd693326f400d1b6861
maetl <me@maetl.net>
2011-07-21 22:18:36 +1000

    change PHP converter to support embedding templates inside a wrapping layout

--- Yarrow/PhpConverter.php
+++ Yarrow/PhpConverter.php
@@ -15,6 +15,20 @@
  * Converter for classic PHP style templating.
  */
 class PhpConverter extends TemplateConverter {
+
+	private $wrappingTemplate = false;
+	
+	/**
+	 * Wraps a layout template around an inner template.
+	 *
+	 * The inner template is captured in the $content variable available
+	 * in the wrapping template.
+	 *
+	 * @param string $template name of template file
+	 */
+	public function wrap($template) {
+		$this->wrappingTemplate = $template;
+	}
 	
 	/**
 	 * Renders a template, applying context variables.
@@ -26,12 +40,25 @@ class PhpConverter extends TemplateConverter {
 	public function render($template, $context) {
 		ob_start();
 		extract($context);
+		
 		$templateFile = $this->getTemplatePath() . '/' . $template;
 		if (file_exists($templateFile)) {
 			include $templateFile;
 		} else {
 			throw new Exception("Template $template not found in {$this->getTemplatePath()}.");
 		}
+		
+		if ($this->wrappingTemplate) {
+			$templateFile = $this->getTemplatePath() . '/' . $this->wrappingTemplate . '.tpl.php';
+			if (file_exists($templateFile)) {
+				$content = ob_get_contents();
+				ob_clean();
+				include $templateFile;
+			} else {
+				throw new Exception("Template {$this->wrappingTemplate} not found in {$this->getTemplatePath()}.");
+			}			
+		}
+		
 		$out = ob_get_contents();
 		ob_clean();
 		return $out;
