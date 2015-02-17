4d9fd969ccc4d891357225e486391f774e28817e
maetl <me@maetl.net>
2015-02-17 23:14:37 +1100

    Server specs for directory indexes and output_dir config

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -3,30 +3,56 @@ require 'spec_helper'
 describe Yarrow::Server do
   include Rack::Test::Methods
 
-  class PwdServer < Yarrow::Server
+  context 'Serves current working directory when config is empty' do
 
-    def config
-      Yarrow::Configuration.new(
-        
-      )
+    class PwdServer < Yarrow::Server
+      def config
+        Yarrow::Configuration.new
+      end
+    end
+
+    let(:app) { PwdServer.new.app }
+
+    it 'serves files' do
+      get '/LICENSE'
+      expect(last_response).to be_ok
+      expect(last_response.content_type).to include('text/plain')
+      expect(last_response.body).to include('THE SOFTWARE')
+    end
+
+    it 'serves a directory index' do
+      get '/'
+      expect(last_response).to be_ok
+      expect(last_response.content_type).to include('text/html')
+      expect(last_response.body).to include('.mtime { text-align:right; }')
     end
 
   end
 
-  let(:app) { PwdServer.new.app }
+  context 'Serves directory specified in config' do
 
-  it 'serves text files from pwd' do
-    get '/LICENSE'
-    expect(last_response).to be_ok
-    expect(last_response.content_type).to include('text/plain')
-    expect(last_response.body).to include('THE SOFTWARE')
-  end
+    class DirectoryServer < Yarrow::Server
+      def config
+        Yarrow::Configuration.new(output_dir: 'spec/fixtures/server/directory')
+      end
+    end
 
-  it 'serves a directory index from pwd' do
-    get '/'
-    expect(last_response).to be_ok
-    expect(last_response.content_type).to include('text/html')
-    expect(last_response.body).to include('.mtime { text-align:right; }')
+    let(:app) { DirectoryServer.new.app }
+
+    it 'serves files' do
+      get '/file.html'
+      expect(last_response).to be_ok
+      expect(last_response.content_type).to include('text/html')
+      expect(last_response.body).to include('<p>file.html</p>')
+    end
+
+    it 'serves a directory index' do
+      get '/'
+      expect(last_response).to be_ok
+      expect(last_response.content_type).to include('text/html')
+      expect(last_response.body).to include('.mtime { text-align:right; }')
+    end
+
   end
 
 end
