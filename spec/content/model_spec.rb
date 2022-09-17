83d5682a02fd834fbe094a9f365633f301186d3c
Mark Rickerby <me@maetl.net>
2022-09-17 23:13:41 +1200

    See if this fixes CI build error

--- spec/content/model_spec.rb
+++ spec/content/model_spec.rb
@@ -20,11 +20,17 @@ describe "yarrow content model" do
 
   xit "loads a model from minimal default config" do
     config_spec = Yarrow::Content::ContentSpec.new(
+      namespace: "",
       model: {
         root: Yarrow::Content::ContentPolicy.new(
-          expansion:
+          expansion: :tree,
+          container: :pages
         )
       }
     )
+
+    model = Yarrow::Content::Model.new(config_spec)
+
+    expect(model.policy_for(:root).expansion).to eq(:tree)
   end
 end
