282d90840566deb95d115d35e4b20c0bd9c2e48c
Mark Rickerby <me@maetl.net>
2023-09-16 16:02:07 +1200

    Clean up fixtures and specs for content formats

--- spec/library/format/format_spec.rb
+++ spec/library/format/format_spec.rb
@@ -9,8 +9,8 @@ describe Yarrow::Format do
         end
       end
 
-      contents_a = Yarrow::Format.read(__dir__ + "/fixtures/custom.abcd")
-      contents_b = Yarrow::Format.read(__dir__ + "/fixtures/custom.abcdefg")
+      contents_a = Yarrow::Format.read(fixture_path("format/custom.abcd"))
+      contents_b = Yarrow::Format.read(fixture_path("format/custom.abcdefg"))
 
       expect(contents_a.document.to_s).to eq("ABCD")
       expect(contents_b.document.to_s).to eq("ABCDEFG")
@@ -21,19 +21,19 @@ describe Yarrow::Format do
 
   describe "Markdown#read" do
     it "can read .md files" do
-      contents = Yarrow::Format.read(__dir__ + "/fixtures/document.md")
+      contents = Yarrow::Format.read(fixture_path("format/document.md"))
 
       expect(contents.document).to be_a(Yarrow::Format::Markdown)
     end
 
     it "can read .markdown files" do
-      contents = Yarrow::Format.read(__dir__ + "/fixtures/document.markdown")
+      contents = Yarrow::Format.read(fixture_path("format/document.markdown"))
 
       expect(contents.document).to be_a(Yarrow::Format::Markdown)
     end
 
     it "can read .htm files" do
-      contents = Yarrow::Format.read(__dir__ + "/fixtures/document.htm")
+      contents = Yarrow::Format.read(fixture_path("format/document.htm"))
 
       expect(contents.document).to be_a(Yarrow::Format::Markdown)
     end
@@ -41,13 +41,13 @@ describe Yarrow::Format do
 
   describe "Yaml#read" do
     it "can read .yml files" do
-      contents = Yarrow::Format.read(__dir__ + "/fixtures/data.yml")
+      contents = Yarrow::Format.read(fixture_path("format/data.yml"))
 
       expect(contents.metadata).to be_a(Yarrow::Format::Yaml)
     end
 
     it "can read .yaml files" do
-      contents = Yarrow::Format.read(__dir__ + "/fixtures/data.yaml")
+      contents = Yarrow::Format.read(fixture_path("format/data.yaml"))
 
       expect(contents.metadata).to be_a(Yarrow::Format::Yaml)
     end
